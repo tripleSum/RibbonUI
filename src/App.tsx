b2989ce0d3f46e96570aa0694b51f10011423e3a
adrian <wengxing2000@foxmail.com>
2023-04-01 23:21:19 +0800

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App: React.FC = () => {
+  return (
+    <div className="APP">
+      <h1>Hello RibbonUI!</h1>
+      <h3>Hello RibbonUI!</h3>
+      <h2>Hello RibbonUI!</h2>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Button/button.tsx
@@ -0,0 +1,11 @@
+export enum ButtonSize {
+  Large = 'large',
+  Small = 'small'
+}
+
+export enum ButtonType {
+  Primary = 'primary',
+  Default = 'default',
+  Danger = 'danger',
+  Link = 'link'
+}
