a05d58a53423f83852027f6150729368e69a4c72
adrian <wengxing2000@foxmail.com>
2023-04-01 23:27:42 +0800

    feat: init

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ const App: React.FC = () => {
     <div className="APP">
       <h1>Hello RibbonUI!</h1>
       <h3>Hello RibbonUI!</h3>
-      <h2>Hello RibbonUI!</h2>
+      <h2>Hello RibbonUI!1</h2>
     </div>
   )
 }
